ba8450a4e629791f05d985cf18f97afe4e3d688f
ElsonJuniorDev <108693146+ElsonJuniorDev@users.noreply.github.com>
2024-09-22 14:01:18 -0300

    --feature add scrollreveal animação

---
+++ script-animacao.js
@@ -0,0 +1,12 @@
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal('.animacao-img',{
+    duration: 1000,
+    delay: 100
+});
+
+revelar.reveal('.animacao-txt',{
+    duration: 2000,
+    origin: 'left'
+ 
+});
